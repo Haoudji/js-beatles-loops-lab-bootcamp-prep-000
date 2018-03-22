443aaded92da5eb5a0342f77ec98a6a0cd4db44c
Issa Boubacar <bacaris78@gmail.com>
2018-03-22 01:08:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicianArray, instrumentsArray){
+  var emptyArray = []
+  for(var i = 0; i < musicianArray.length; i++){
+    emptyArray.push( musicianArray[i] + ' plays ' + instrumentsArray[i] )
+  }
+  return emptyArray
+}
+function johnLennonFacts(array){
+  var n = 0;
+  while(n < array.length){
+     array[n] + "!!! "
+    n++
+  }
+  return array
+}
